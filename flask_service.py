cab36645f5f206e5d465c5e2b30b36b935e9ae10
Daria0803 <dariana0803@mail.ru>
2022-12-13 19:08:35 +0300

    service on flask - please add this file to spert directory with neural model

---
+++ flask_service.py
@@ -0,0 +1,76 @@
+from spert.spert_trainer import SpERTTrainer
+
+# import flask as fl
+from flask import Flask, request, json, jsonify
+from flask_cors import CORS, cross_origin
+
+import argparse
+
+from args import train_argparser, eval_argparser, predict_argparser
+from config_reader import process_configs
+from spert import input_reader
+from spert.spert_trainer import SpERTTrainer
+from config_reader import _yield_configs
+from transform_json import prepare_another_json, contextCreate_v2
+
+app = Flask(__name__)
+
+#trainer = None
+#model = None
+
+def __load_model(run_args):
+    global model
+    global trainer
+
+    trainer = SpERTTrainer(run_args)
+    model = trainer.load_model()
+    print('hi from _load_model')
+
+@app.route('/', methods=['GET', 'POST'])
+def getTextFromReactReturnJson():
+    if request.method == 'POST':
+        text = request.get_data().decode("utf-8")
+        with open('predicts/input_prediction_example_1.json', 'w') as f:
+            f.write('[' + text + ']')
+
+        for run_args, _run_config, _run_repeat in _yield_configs(arg_parser, args):
+            #__predict(run_args)
+            trainer.predict(model, dataset_path=run_args.dataset_path, types_path=run_args.types_path,
+                            input_reader_cls=input_reader.JsonPredictionInputReader)
+
+        newJson = prepare_another_json()
+        return jsonify({'data': newJson})
+    else:
+        print(request)
+        return request.get_data()
+
+'''
+def _predict(target):
+    arg_parser = predict_argparser()
+    process_configs(target=target, arg_parser=arg_parser)
+'''
+
+def __predict(run_args):
+    #global model
+    #global trainer
+    #trainer = SpERTTrainer(run_args)
+    #model = trainer.load_model()
+    trainer.predict(model, dataset_path=run_args.dataset_path, types_path=run_args.types_path,
+                    input_reader_cls=input_reader.JsonPredictionInputReader)
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(add_help=False)
+    arg_parser.add_argument('mode', type=str, help="Mode: 'train' or 'eval'")
+    args, _ = arg_parser.parse_known_args()
+
+    arg_parser = predict_argparser()
+    args, _ = arg_parser.parse_known_args()
+    for run_args, _run_config, _run_repeat in _yield_configs(arg_parser, args):
+        trainer = SpERTTrainer(run_args)
+        model = trainer.load_model()
+
+    app.run(debug=False)
+    #app.run(host='127.0.0.1', port=3000, threaded = True, debug=True)
+
+#flask_cors.CORS(app, expose_headers='Authorization')
